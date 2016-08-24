43043c4809249e2267144029d231954bda229932
Alistair Madden <Alistair.John.Madden@gmail.com>
2016-08-24 22:36:34 +0100

    Set up basic homepage using static express routing and simple bootstrap template
    Bootstrap template: http://getbootstrap.com/examples/jumbotron/
    Express routing: https://expressjs.com/en/starter/basic-routing.html

--- app.js
+++ app.js
@@ -5,8 +5,11 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));
+app.use(express.static('node_modules'));
+
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.sendFile('/public/index.html');
 });
 
 app.listen(3000, function () {
