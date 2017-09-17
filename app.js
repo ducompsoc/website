079b8f8d425f5875fd99ca616233565080aaca8d
Alistair Madden <alistair.john.madden@gmail.com>
2017-09-17 12:36:28 +0100

    Implement email sending form on back end
    Uses Nodemailer to send an email to ourselves with the query/comment/whatever that was typed into the form on the email. Should be pretty hard to programatically hit this route as captcha is required.
    
    To do: Validate that user has entered information into all fields before allowing them to send. Implement on front and back end.
    
    Note: For security, sensitive information has been moved to separate JSON file. Make sure to upload this file to server when deploying.

--- app.js
+++ app.js
@@ -2,7 +2,27 @@
  * Created by Alistair on 20/08/2016.
  */
 
-var express = require('express');
+const express = require('express'),
+    bodyParser = require('body-parser'),
+    https = require('https'),
+    querystring = require('querystring'),
+    fs = require('fs'),
+    nodemailer = require('nodemailer'),
+    appDetails = JSON.parse(fs.readFileSync('./unimportantFile.json', 'utf-8'));
+
+const poolConfig = {
+    pool: true,
+    host: appDetails["email-host"],
+    port: appDetails["email-port"],
+    secure: false, // STARTTLS
+    auth: {
+        user: appDetails["email-user"],
+        pass: appDetails["email-password"]
+    }
+};
+
+const transporter = nodemailer.createTransport(poolConfig);
+
 var app = express();
 
 // Remove information about the server for security reasons
@@ -24,6 +44,13 @@ app.set('view engine', 'pug');
  */
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
+
+/**
+ * Set up urlencodedParser function to use as middleware.
+ *
+ * reference:   https://github.com/expressjs/body-parser
+ */
+var urlencodedParser = bodyParser.urlencoded({ extended:false });
 
 /**
  * Handles any HTTP GET requests to /. If the user navigates to /, res.render is called which renders the index view
@@ -61,7 +88,73 @@ app.get('/blog', function (req, res) {
     res.render('blog');
 });
 
+/**
+ * Parse body and run reCAPTCHA authentication. If successful, use nodemailer to send an email to
+ * computing.society@durham.ac.uk with the user's message.
+ */
+app.post('/api/contact', urlencodedParser, captchaAuth, function (req, res) {
+    res.set('Content-Type', 'text/plain');
 
+    // setup email data with unicode symbols
+    const mailOptions = {
+        from: 'computing.society@durham.ac.uk',
+        to: 'computing.society@durham.ac.uk', // list of receivers
+        subject: req.body["subject"], // Subject line
+        text: "From: " + req.body["realname"] + " - " + req.body["email"] + "\n\n" +
+            req.body["msgbody"] // plain text body
+    };
+
+    transporter.sendMail(mailOptions, function (err, info) {
+        if(err) {
+            console.error(err);
+            res.sendStatus(500);
+        }
+        else {
+            res.sendStatus(200);
+        }
+    });
+
+    res.sendStatus(200);
+});
+
+function captchaAuth(req, res, next) {
+
+    const postData = querystring.stringify({
+        secret: appDetails["g-recaptcha-secret"],
+        response: req.body["g-recaptcha-response"]
+    });
+
+    const captchaReqOptions = {
+        hostname: "google.com",
+        port: 443,
+        path: "/recaptcha/api/siteverify",
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Content-Length": Buffer.byteLength(postData)
+        }
+    };
+
+    const captchaReq = https.request(captchaReqOptions, function(response) {
+        response.on("data", function (data) {
+            var jsonObject = JSON.parse(data);
+            if(jsonObject["success"] === true) {
+                next();
+            }
+            else {
+                res.sendStatus(403);
+            }
+
+        });
+    });
+
+    captchaReq.on("error", function () {
+        res.sendStatus(500);
+    });
+
+    captchaReq.write(postData);
+    captchaReq.end();
+}
 
 // Start listening on port 9000 -- if running locally navigate to localhost:9000 in a browser to see the results!
 app.listen(9000, function () {
