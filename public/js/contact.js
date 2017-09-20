01ee006de01f2b2be6c26b0bf30077a10827bf58
Alistair Madden <alistair.john.madden@gmail.com>
2017-09-20 23:24:02 +0100

    Implement invisible reCAPTCHA on front end
    The reCAPTCHA v2 div was not scaling well to mobile devices. This is a more elegant solution. This change also implements email validation of the form as it was convenient when setting up invisible reCAPTCHA. See the second example "Invoking the invisible reCAPTCHA challenge after client side validation.": https://developers.google.com/recaptcha/docs/invisible#example

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,6 +1,12 @@
+// Variable to hold request
+var request;
+
 $(function () {
-    // Variable to hold request
-    var request;
+
+    var $inputs;
+
+    const emailRegex = new RegExp("^(?=[A-Z0-9][A-Z0-9@._%+-]{5,253}$)[A-Z0-9._%+-]{1,64}@(?:(?=[A-Z0-9-]{1,63}\.)" +
+        "[A-Z0-9]+(?:-[A-Z0-9]+)*\.){1,8}[A-Z]{2,63}$", "i");
 
 // Bind to the submit event of our form
     $("#send-email").submit(function(event){
@@ -12,46 +18,73 @@ $(function () {
         if (request) {
             request.abort();
         }
+
         // setup some local variables
-        var $form = $(this);
+        var $form = $("#send-email");
 
         // Let's select and cache all the fields
-        var $inputs = $form.find("input, select, button, textarea");
+        $inputs = $form.find("input, textarea");
 
-        // Serialize the data in the form
-        var serializedData = $form.serialize();
-
-        // Let's disable the inputs for the duration of the Ajax request.
-        // Note: we disable elements AFTER the form data has been serialized.
-        // Disabled form elements will not be serialized.
-        $inputs.prop("disabled", true);
-
-        // Fire off the request to /api/contact
-        request = $.ajax({
-            url: "/api/contact",
-            type: "post",
-            data: serializedData
-        });
-
-        // Callback handler that will be called on success
-        request.done(function (response, textStatus, jqXHR){
-        });
-
-        // Callback handler that will be called on failure
-        request.fail(function (jqXHR, textStatus, errorThrown){
-            // Log the error to the console
-            console.error(
-                "The following error occurred: "+
-                textStatus, errorThrown
-            );
-        });
-
-        // Callback handler that will be called regardless
-        // if the request failed or succeeded
-        request.always(function () {
-            // Reenable the inputs
-            $inputs.prop("disabled", false);
-        });
+        validate();
 
     });
+
+    function validate() {
+        if (!emailRegex.test($inputs["1"]["value"]) === true) {
+            $(".form-error-message").css("display", "block");
+        } else {
+            grecaptcha.execute();
+        }
+    }
 });
+
+var successfulCaptcha = function () {
+    console.log("success!");
+    $(".captcha-error-message").css("display", "none");
+
+    // setup some local variables
+    var $form = $("#send-email");
+
+    // Let's select and cache all the fields
+    var $inputs = $form.find("input, textarea");
+
+    // Serialize the data in the form
+    var serializedData = $form.serialize();
+
+    console.log(serializedData);
+
+    // Let's disable the inputs for the duration of the Ajax request.
+    // Note: we disable elements AFTER the form data has been serialized.
+    // Disabled form elements will not be serialized.
+    $inputs.prop("disabled", true);
+
+    // Fire off the request to /api/contact
+    request = $.ajax({
+        url: "/api/contact",
+        type: "post",
+        data: serializedData,
+        statusCode: {
+            403: function () {
+                $(".captcha-error-message").css("display", "block");
+            }
+        }
+    });
+
+    // Callback handler that will be called on success
+    request.done(function (response, textStatus, jqXHR){
+        $(".success-message").css("display", "block");
+    });
+
+    // Callback handler that will be called on failure
+    request.fail(function (jqXHR, textStatus, errorThrown){
+        // Log the error to the console
+        console.error(textStatus + " " + errorThrown);
+    });
+
+    // Callback handler that will be called regardless
+    // if the request failed or succeeded
+    request.always(function () {
+        // Reenable the inputs
+        $inputs.prop("disabled", false);
+    });
+};
