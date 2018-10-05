a0d74a1f3dee9e6dccb722b04aa6d89688cb3af9
Robbie Jakob-Whitworth <robzwolf@gmail.com>
2018-10-05 14:34:49 +0100

    Dynamically load events on events page from JSON

---
+++ public/js/load_events.js
@@ -0,0 +1,49 @@
+// load_events.js
+// Loads the events and renders them on the page
+
+$(document).ready(() => {
+
+    const generateEventHTML = ((event, type) => {
+        let eventHTML = `<div class="row">
+            <img src="${event.image_url}" />
+            <a class="btn btn-info btn-block" href="#${event.shortname}" data-toggle="collapse">${event.name}</a>
+            <div class="collapse ${type === 'upcoming' ? 'in' : ''}" id="${event.shortname}">`;
+
+        event.text.forEach((line) => {
+            eventHTML += `<p>${line}</p>`;
+        });
+
+        if (event.more_info_url) {
+            let more_info_text = '';
+            if (event.more_info_url.indexOf('https://www.facebook.com/') === 0 || event.more_info_url.indexOf('https://facebook.com/') === 0) {
+                more_info_text = `Facebook Event for ${event.name}`;
+            } else {
+                more_info_text = `More info about ${event.name}`;
+            }
+            eventHTML += `<a href="${event.more_info_url}">&raquo; ${more_info_text}</a>`;
+        }
+
+        eventHTML += '</div></div>';
+
+        return eventHTML;
+    });
+
+    $.getJSON('/data/events.json', (data) => {
+        console.log(data);
+
+        // Render the upcoming events
+        data.events.upcoming.forEach((event) => {
+            $('#upcoming_events').append(generateEventHTML(event, 'upcoming'));
+        });
+
+        // Render the past events
+        data.events.past.forEach((event) => {
+            $('#past_events').append(generateEventHTML(event, 'past'));
+        });
+
+        $('#upcoming_events img, #past_events img').on('load', () => {
+            $('.row.loading-gif').hide();
+            $('#upcoming_events, #past_events').removeClass('hidden');
+        });
+    });
+});
